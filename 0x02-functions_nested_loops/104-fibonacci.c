06e290861b6f977cdaa579ff9cbb37242c2be5db
simo <simobelouarraq5@gmail.com>
2022-12-13 16:25:19 -0800

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,79 +1,38 @@
 #include <stdio.h>
-
+#define LARGEST 10000000000
 /**
-
- * print_fib - prints the first 50 Fibonacci numbers,
-
- * starting with 1 and 2, followed by a new line.
-
- * @len: the length of with the fib sequance should print to.
-
- *
-
- * Return: void
-
+ * main - main block
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ * Numbers should be coma and space separated.
+ * Return: 0
  */
-
-void print_fib(int len)
-
-{
-
-	unsigned int target, i;
-
-	unsigned int former_l, former_r,;
-
-	unsigned int latter_l, latter_r;
-
-	unsigned int fib_l, fib_r;
-
-	f = 1;
-
-	l = 2;
-
-	target = len - 2;
-
-	for (i = 0 ; i < target ; i++)
-
-	{
-
-		fib = f + l;
-
-		printf("%d : ", i + 4);
-
-		printf("%.0Lf" , fib);
-
-		f = l;
-
-		l = fib;
-
-		if (i < target - 1)
-
-			printf("\n");
-
-	}
-
-	putchar('\n');
-
-}
-
-/**
-
- * main - Entry point,
-
- *
-
- *
-
- * Return: Always 0 (sucess)
-
- */
-
 int main(void)
-
 {
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-	print_fib(98);
-
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
+	{
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
+	}
+	printf("\n");
 	return (0);
-
 }
