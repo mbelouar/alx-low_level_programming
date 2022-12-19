1feb4b6b3f54eef33b56b06fa83c43873c791387
simo <simobelouarraq5@gmail.com>
2022-12-19 07:42:23 -0800

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,25 +4,25 @@
  * rev_string - reverses a string
  * @s: string to be reversed
  */
-
 void rev_string(char *s)
 {
 	char tmp;
-	int i, final_len, len;
+	int i, len, len1;
 
 	len = 0;
-	while (s[len])
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
 		len++;
 	}
-	
-	final_len = len - 1;
-	i = 0;
-	while (i < len)
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[final_len];
-		s[final_len] = tmp;
-		final_len--;
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
