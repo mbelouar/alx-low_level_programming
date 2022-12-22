4e01188f055cdbb741da5cb906e2e170a0e264a0
simo <simobelouarraq5@gmail.com>
2022-12-22 15:02:41 -0800

    touppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,11 +12,11 @@ char *string_toupper(char *s)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (str[i] >= 97 && str[i] <= 122)
-			str[i] = str[i] - 32;
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
 		i++;
 	}
-	return (str);
+	return (s);
 }
