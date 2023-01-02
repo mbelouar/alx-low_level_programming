8064ebda4acf198641ccb8546b682c52f52423a6
simo <simobelouarraq5@gmail.com>
2023-01-02 15:49:11 -0800

    memcpy

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,15 @@
  * Return: If c is found - a pointer to the first occurence.
  *         If c is not found - NULL.
  */
+
 char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
 	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			return (&s[i]);
+	while (s[i] && s[i] != c)
 		i++:
-	}
+	if (s[i] == c)
+		return (&s[i]);
 	return (NULL);
 }
