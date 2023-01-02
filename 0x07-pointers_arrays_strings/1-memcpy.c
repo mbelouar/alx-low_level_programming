70712a345f67c510de85bcc9caa84051ea11bd6f
simo <simobelouarraq5@gmail.com>
2023-01-02 15:42:14 -0800

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,11 +13,14 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0 ;
-	while (i < n)
+	i = 0;
+	while (i < n)  /*Delacring WHILE*/
 	{
-		dest[i] = src[i];
+		*(dest + i) = *(src + i); /*add 1 position dest and src*/
 		i++;
-	}
+
+	} /*END WHILE*/
+
+
 	return (dest);
 }
