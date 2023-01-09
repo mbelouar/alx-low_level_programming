22d2519352532198a85380f344d6705f63b7c8b2
Mohammed Belouarraq <mohammed.belouarraq@etu.uae.ac.ma>
2023-01-09 13:25:57 -0800

    malloc_free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,16 +7,16 @@
  * Return: Length of the string.
  */
 
-int     _strlen(char *str)
+int	_strlen(char *str)
 {
-        int i;
-
-        i = 0;
-        while (str[i])
-        {
-                i++;
-        }
-        return (i);
+	int	i;
+	
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
