cb800f540540a1f6439f2ea44c93c47a48aa555d
Mohammed Belouarraq <mohammed.belouarraq@etu.uae.ac.ma>
2023-01-09 13:23:17 -0800

    malloc_free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,23 +1,30 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - function that returns the length of a string.
+ * @str: string to calculate his length.
+ *
+ * Return: Length of the string.
+ */
+
+int     _strlen(char *str)
+{
+        int i;
+
+        i = 0;
+        while (str[i])
+        {
+                i++;
+        }
+        return (i);
+}
+
+/**
  * _strdup - function that returns a pointer to a newly allocated string.
  * @str: string to allocate memory for.
  *
  * Return: Pointer to string in memory.
  */
-
-int	_strlen(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-	return (i);
-}
 
 char	*_strdup(char *str)
 {
