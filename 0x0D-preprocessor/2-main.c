8f0cbb9456765eb69d4f757f876de7026c6102c7
Mohammed Belouarraq <mohammed.belouarraq@etu.uae.ac.ma>
2023-01-16 17:04:06 -0800

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: 0 (SUCESS)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H
+#define _3_FUNCTION_LIKE_MACRO_H
+
+#define ABS(X) ((X) < 0 ? (-X) : (X))
+
+#endif
